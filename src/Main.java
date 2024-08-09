67ec794ef0158d8ea65c4952cfd1af03480704d5
eduarda-manentii <dudamanenti65@gmail.com>
2024-08-08 22:13:06 -0300

    Criado classe de carro.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import aula1_introducao.Carro;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Carro carro = new Carro();
+        carro.marca = "Toyota";
+        carro.modelo = "Iares";
+        carro.ano = 2020;
+        carro.cor = "Preto";
+        carro.preco = 9000.0;
+        carro.exibirInformacoes();
     }
 
 }
--- src/aula1_introducao/Carro.java
+++ src/aula1_introducao/Carro.java
@@ -1,5 +1,37 @@
 package aula1_introducao;
 
 public class Carro {
+   public String marca;
+   public String modelo;
+   public Integer ano;
+   public String cor;
+   public Double preco;
+
+   public Carro() {}
+
+   public Carro(String marca, String modelo, Integer ano, String cor, Double preco) {
+      this.marca = marca;
+      this.modelo = modelo;
+      this.ano = ano;
+      this.cor = cor;
+      this.preco = preco;
+   }
+
+   public void acelerar() {
+      System.out.println("O carro está acelerando");
+   }
+
+   public void frear() {
+      System.out.println("O carro está freando");
+   }
+
+   public void exibirInformacoes() {
+       System.out.println("Carro{" +
+              "marca='" + marca + '\'' +
+              ", modelo='" + modelo + '\'' +
+              ", ano=" + ano +
+              ", cor='" + cor + '\'' +
+              ", preco=" + preco);
+   }
 
 }
